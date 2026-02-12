55cb9777ce26a257836dcc93aa525517c23f5922
BentPen <bentpen97@gmail.com>
2026-02-11 20:22:53 -0500

    migrate to bevy-cli

---
+++ src_precli/main.rs
@@ -0,0 +1,34 @@
+use bevy::asset::AssetMetaCheck;
+use bevy::post_process::bloom::Bloom;
+use bevy::prelude::*;
+
+mod camera;
+mod control;
+mod lights;
+mod player;
+
+use camera::*;
+use lights::*;
+use player::PlayerPlugin;
+
+fn setup_camera(mut commands: Commands) {
+    // commands.spawn(Camera2d);
+    commands.spawn((Camera2d, Bloom::NATURAL));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            meta_check: AssetMetaCheck::Never,
+            file_path: "src/assets".into(),
+            ..default()
+        }))
+        .insert_resource(ClearColor(Color::BLACK)) // We have updated the bg color to white
+        .add_systems(
+            Startup,
+            (setup_main_tile, setup_main_light, setup_camera).chain(),
+        )
+        .add_systems(Update, (update_camera, update_main_light).chain())
+        .add_plugins(PlayerPlugin)
+        .run();
+}
